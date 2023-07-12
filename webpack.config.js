9b29c6a631f881c06ec323563394569287f4ac0e
riskmaris <mariskaps20@gmail.com>
2023-07-12 13:43:34 +0300

    make changes

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      title: 'development',
+    }),
+  ],
+};
