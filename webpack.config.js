45e760be04b58f7b32a2383117b8bc2a1fc1c413
riskmaris <mariskaps20@gmail.com>
2023-07-12 20:03:51 +0300

    make changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,3 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//     clean: true,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/i,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       title: 'ToDo-List',
-//       filename: 'index.html',
-//     }),
-//   ],
-// };
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
