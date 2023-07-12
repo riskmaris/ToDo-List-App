1f21bae936c231e33216b33a9924c6acd47d5f8a
riskmaris <mariskaps20@gmail.com>
2023-07-12 15:30:32 +0300

    fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,50 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: 'ToDo-List',
+//       filename: 'index.html',
+//     }),
+//   ],
+// };
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'src'), // Set the static directory to 'src'
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -15,12 +54,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'ToDo-List',
-      filename: 'index.html',
-    }),
-  ],
 };
