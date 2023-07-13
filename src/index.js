00f43cf3166082679af6630e7d94445ec03a8a0d
riskmaris <mariskaps20@gmail.com>
2023-07-13 10:06:55 +0300

    make changes

--- src/index.js
+++ src/index.js
@@ -1,40 +1,78 @@
 // import './styles.css';
 
-const toDoTasks = [
-  {
-    description: 'Walk the dog',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Take breakfast',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Complete my project',
-    completed: false,
-    index: 2,
-  },
-];
+// const toDoTasks = [
+//   {
+//     description: 'Walk the dog',
+//     completed: false,
+//     index: 0,
+//   },
+//   {
+//     description: 'Take breakfast',
+//     completed: true,
+//     index: 1,
+//   },
+//   {
+//     description: 'Complete my project',
+//     completed: false,
+//     index: 2,
+//   },
+// ];
 
-// difining the UL id
-const listItem = document.getElementById('list-item');
+// // difining the UL id
+// const listItem = document.getElementById('list-item');
 
-// loop through the array of obj and creat li for each
-toDoTasks.forEach((todo) => {
-  // Create a new li element
-  const listElement = document.createElement('li');
-  const spanElement = document.createElement('span');
-  spanElement.textContent = todo.description;
+// // loop through the array of obj and creat li for each
+// toDoTasks.forEach((todo) => {
+//   // Create a new li element
+//   const listElement = document.createElement('li');
+//   const spanElement = document.createElement('span');
+//   spanElement.textContent = todo.description;
 
-  const checkboxElement = document.createElement('input');
-  checkboxElement.type = 'checkbox';
-  listElement.appendChild(checkboxElement);
-  listElement.appendChild(spanElement);
+//   const checkboxElement = document.createElement('input');
+//   checkboxElement.type = 'checkbox';
+//   listElement.appendChild(checkboxElement);
+//   listElement.appendChild(spanElement);
 
-  if (todo.completed) {
-    listItem.classList.add('completed');
+//   if (todo.completed) {
+//     listItem.classList.add('completed');
+//   }
+//   listItem.appendChild(listElement);
+// });
+
+
+import './style.css';
+import UI from './modules/UI.js';
+import Store from './modules/localstorage.js';
+
+// Event for Displaying the to do tasks
+document.addEventListener('DOMContentLoaded', UI.displayToDoList);
+
+// Event for adding new to do tasks
+document.querySelector('#todo-form').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    UI.getNewToDoTask();
   }
-  listItem.appendChild(listElement);
+});
+
+// Event for deleting new item for the list
+document.querySelector('#list-item').addEventListener('click', (e) => {
+  Store.removetodoTask(e.target);
+  UI.deletetodoTask(e.target);
+});
+
+// Event for editing the to do list
+document.querySelector('#list-item').addEventListener('click', (e) => {
+  if (e.target.classList.contains('bi-pencil-square')) {
+    UI.editToDoTask(e.target);
+  }
+});
+
+// Event for updating tasks
+document.querySelector('#list-item').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && e.target.classList.contains('edit-list')) {
+    e.preventDefault();
+    Store.updatetodoTask(e.target);
+    window.location.reload();
+  }
 });
---
+++ src/modules/toDo.js
@@ -0,0 +1,11 @@
+// Todo Class: To represent a Task
+
+class ToDo {
+    constructor(todo) {
+      this.desc = todo;
+      this.completed = false;
+      this.index = 0;
+    }
+  }
+  
+  export default ToDo;
